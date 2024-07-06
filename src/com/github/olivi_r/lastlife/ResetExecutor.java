201d569e0de6dc3942a54090a8e85d3ecca9ab4f
Olivia Ryan <olivia.r.dev@gmail.com>
2024-07-06 05:15:33 +0100

    Dont recreate lives namespaced keyz

--- src/com/github/olivi_r/lastlife/ResetExecutor.java
+++ src/com/github/olivi_r/lastlife/ResetExecutor.java
@@ -1,7 +1,6 @@
 package com.github.olivi_r.lastlife;
 
 import org.bukkit.Bukkit;
-import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,9 +15,8 @@ public class ResetExecutor implements CommandExecutor {
 	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		NamespacedKey key = new NamespacedKey(plugin, "lives");
 		for (Player p : Bukkit.getOnlinePlayers()) {
-			p.getPersistentDataContainer().remove(key);
+			p.getPersistentDataContainer().remove(plugin.livesKey);
 		}
 		plugin.refreshTeams();
 		return true;
